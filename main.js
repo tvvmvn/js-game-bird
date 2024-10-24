d20d9a3588374ecbf68477206c3032869a4affba
tvvmvn <tvvmvn@gmail.com>
2024-10-24 12:40:22 +0900

    10.24

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
 // canvas
 var canvas = document.getElementById("myCanvas");
 canvas.width = 500;
-canvas.height = 300;
-canvas.style["backgroundColor"] = "#f1f1f1";
+canvas.height = 375;
 var ctx = canvas.getContext("2d");
 
-// class
-class Intro {
-  render() {
-    ctx.font = "30px Monospace";
-    ctx.fillStyle = "#0bf";
-    ctx.textAlign = "center";
-    ctx.fillText("Flappy Bird", canvas.width / 2, 130);
+class Background extends Image {
+  constructor() {
+    super();
+    this.src = "bg.png";
+  }
 
-    ctx.font = "16px Monospace";
-    ctx.fillStyle = "#000";
-    ctx.textAlign = "center";
-    ctx.fillText("Press any key to start", canvas.width / 2, 160);
+  render() {
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, 0, 0, this.width, this.height, 0, 0, canvas.width, canvas.height);
   }
 }
 
-class Actor {
-  width = 30;
-  height = 30;
+// class
+class Intro extends Image {
+  constructor() {
+    super();
+    this.src = "logo.png";
+    this.width /= 3;
+    this.height /= 3;
+  }
+
+  render() {
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, canvas.width / 2 - (this.width / 2), (canvas.height / 2) - this.height, this.width, this.height);
+  }
+}
+
+class StartButton extends Image {
+  constructor() {
+    super();
+    this.src = "start.png";
+    this.width /= 3;
+    this.height /= 3;
+  }
+
+  render() {
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, canvas.width / 2 - (this.width / 2), (canvas.height / 2) + this.height, this.width, this.height);
+  }
+}
+
+class Actor extends Image {
+  // src  
   x = 150;
   y = 100;
   color = "#0bf";
+  s = 0;
+  
+  constructor() {
+    super();
+    this.src = "flap_up.png";
+  }
 
   fall() {
     if (this.y > canvas.height) {
@@ -44,26 +74,49 @@ class Actor {
   }
 
   render() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, this.x, this.y, this.width, this.height);
+    
+    this.s++;
+
+    if (this.s % 10 == 0) {
+      switch (this.src) {
+        case location.origin + "/flap_up.png":
+          this.src = location.origin + "/flap_center1.png";
+          break;
+        case location.origin + "/flap_center1.png":
+          this.src = location.origin + "/flap_down.png";
+          break;
+        case location.origin + "/flap_down.png":
+          this.src = location.origin + "/flap_center2.png";
+          break;
+        case location.origin + "/flap_center2.png":
+          this.src = location.origin + "/flap_up.png";
+          break;
+      }
+    }
   }
 }
 
-class Obstacle {
-  width = 40;
-  totalHeight = 200;
-  height1 = Math.random() * this.totalHeight;
-  height2 = this.totalHeight - this.height1;
+class Obstacle extends Image {
   x = canvas.width;
-  y1 = 0
-  y2 = canvas.height - this.height2;
-  color = "green";
+  y = -242 + (Math.random() * 160);
   passed = false;
+  spaceHeight = 127;
+  eachHeight = 286; 
+
+  constructor() {
+    super();
+    this.src = "pipe.png";
+  }
 
   collisionDetection(actor) {
+    var ceiling = this.eachHeight + this.y;
+
     if (
-      this.x < actor.x + actor.width && this.x + this.width > actor.x
-      && (this.y1 + this.height1 > actor.y || this.y2 < actor.y + actor.height)
+      actor.x + actor.width > this.x 
+      && actor.x < this.x + this.width
+      && (actor.y < ceiling || actor.y + actor.height > ceiling + this.spaceHeight)
     ) {
       return true;
     }
@@ -76,13 +129,12 @@ class Obstacle {
   }
 
   render() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y1, this.width, this.height1);
-    ctx.fillRect(this.x, this.y2, this.width, this.height2);
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, this.x, this.y, this.width, this.height);
   }
 }
 
-class Score {
+class Score extends Image {
   value = 0;
 
   add() {
@@ -91,31 +143,36 @@ class Score {
 
   render() {
     ctx.font = "16px Monospace";
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#fff";
     ctx.textAlign = "center";
-    ctx.fillText("score: " + this.value, canvas.width - 80, 40);
+    ctx.fillText("Score " + this.value, canvas.width - 80, 40);
   }
 }
 
-class GameOver {
+class GameOver extends Image {
+  constructor() {
+    super();
+    this.src = "over.png";
+    this.width /= 3;
+    this.height /= 3;
+  }
+
   render() {
-    ctx.font = "20px Monospace";
-    ctx.fillStyle = "#000";
-    ctx.textAlign = "center";
-    ctx.fillText("GAME OVER ⏩️", canvas.width / 2, canvas.height / 2);
-    // ctx.globalCompositeOperation = "";
+    // drawImage(image, dx, dy, dwidth, dheight)
+    ctx.drawImage(this, canvas.width / 2 - (this.width / 2), canvas.height / 2 - (this.height / 2), this.width, this.height);
   }
 }
 
 class Game {
+  bg = new Background();
   intro = new Intro();
+  startBtn = new StartButton();
   actor = new Actor();
   score = new Score();
   gameOver = new GameOver();
   obstacles = [];
   frameNo = 0;
   gravity = 0;
-  inputable = true;
   timer;
   start;
   over = false;
@@ -133,9 +190,13 @@ class Game {
   actionPerformed() {
     this.clearScreen();
 
-    // Show intro
+    // Background
+    this.bg.render();
+
+    // Title
     if (!this.start) { 
       this.intro.render();
+      this.startBtn.render();
       return;
     }
 
@@ -170,10 +231,10 @@ class Game {
 
         if (this.actor.x > obstacle.x + obstacle.width) {
           this.score.add();
-
+          
           var s = new Audio("sfx_point.wav");
           s.play();
-
+          
           obstacle.passed = true;
         }
       }
@@ -185,39 +246,28 @@ class Game {
     // Score
     this.score.render();
 
+    // Game over
     if (this.over) {
       clearInterval(this.timer);
       this.gameOver.render();
     }
   }
 
-  keyDownHandler(key) {
+  clickHandler(e) {
     if (this.over) {
       game = new Game(true);
     } else {
       if (!this.start) {
         this.start = true;
       } else {
-        if (key == ' ') {
-          if (this.inputable) {
-            var s = new Audio("sfx_wing.wav");
-            s.play();
+        var s = new Audio("sfx_wing.wav");
+        s.play();
 
-            this.gravity = -3;
-            this.inputable = false;
-          }
-        }
+        this.gravity = -3;
       }
-    }
-  }
-
-  keyUpHandler(key) {
-    if (key == ' ') {
-      this.inputable = true;
     }
   }
 }
 
 var game = new Game(false);
-document.addEventListener("keydown", (e) => game.keyDownHandler(e.key));
-document.addEventListener("keyup", (e) => game.keyUpHandler(e.key));
+canvas.addEventListener("mousedown", (e) => game.clickHandler(e))
